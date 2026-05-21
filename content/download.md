ab70a473c8b6bed2172f32f16d9ee06367f1b808
shzhxh <shzhxh@163.com>
2026-05-21 17:11:45 +0800

    添加新版网站:面向社区用户、开发者和贡献者

---
+++ content/download.md
@@ -0,0 +1,4 @@
++++
+title = "下载"
+template = "download.html"
++++
--- static/js/site.js
+++ static/js/site.js
@@ -1,98 +1,64 @@
-// function scrollTo(offsetTop) {
-//     const parent = document.querySelector('html')
-//     console.log(parent);
-//     parent.scrollTo({
-//         top: offsetTop,
-//         behavior: 'smooth'
-//     })
-// }
-
-// function isWeixinBrowser() {
-//     var ua = navigator.userAgent.toLowerCase();
-//     if (ua.indexOf("micromessenger") != -1) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-function documentReady() {
-    // https://bulma.io/documentation/components/navbar/#navbar-menu
-
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-        // Add a click event on each of them
-        $navbarBurgers.forEach(el => {
-            el.addEventListener('click', () => {
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarBurger = document.querySelector('.navbar-burger');
+    const navMenu = document.getElementById('navMenu');
+    
+    navbarBurger.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+    
+    document.querySelectorAll('.navbar-item').forEach(item => {
+        item.addEventListener('click', function() {
+            navMenu.classList.remove('active');
+        });
+    });
+    
+    const filterBtns = document.querySelectorAll('.filter-btn');
+    const caseCards = document.querySelectorAll('.case-card');
+    
+    filterBtns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            filterBtns.forEach(b => b.classList.remove('active'));
+            this.classList.add('active');
+            
+            const filter = this.dataset.filter;
+            
+            caseCards.forEach(card => {
+                if (filter === 'all' || card.dataset.category === filter) {
+                    card.style.display = 'block';
+                } else {
+                    card.style.display = 'none';
+                }
             });
         });
-    }
-
-    // const $menu_items = document.querySelectorAll('.menu-item')
-    // if ($menu_items.length > 0) {
-    //     $menu_items.forEach((item) => {
-    //         item.addEventListener('click', (e) => {
-    //             e.preventDefault()
-    //             let targetEl = document.querySelector(item.hash);
-    //             if (isWeixinBrowser()) {
-    //                 document.querySelector('html').scrollTop = targetEl.offsetTop
-    //             } else {
-    //                 scrollTo(targetEl.offsetTop)
-    //             }
-
-    //         })
-    //     })
-    // }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
-if (document.readyState === 'loading') {
-    // Loading hasn't finished yet
-    document.addEventListener('DOMContentLoaded', documentReady);
-} else {
-    // `DOMContentLoaded` has already fired
-    documentReady();
-}
-const titleIndentation = {
-    H2: 1,
-    H3: 2,
-    H4: 3,
-    H5: 4,
-    H6: 5,
-    H7: 6,
-    H8: 7,
-}
-function traverse(node) {
-    if (node.nodeType === Node.ELEMENT_NODE) {
-        // 处理当前节点
-        if (titleIndentation[node.tagName]) {
-            node.style.marginLeft = titleIndentation[node.tagName] + 'em'
-        } else if (node.previousElementSibling && titleIndentation[node.previousElementSibling.tagName]) {
-            node.style.marginLeft = titleIndentation[node.previousElementSibling.tagName] + 3 + 'em';
-        } else if (node.previousElementSibling && !titleIndentation[node.previousElementSibling.tagName]) {
-            node.style.marginLeft = node.previousElementSibling.style.marginLeft;
-        }
-
-        // 遍历子节点
-        for (let i = 0; i < node.childNodes.length; i++) {
-            traverse(node.childNodes[i]);
-        }
-    }
-}
-
-const userAgent = window.navigator.userAgent;
-const screenWidth = window.innerHeight || document.documentElement.clientWidth || document.body.clientWidth
-const isMobile = /Mobile/i.test(userAgent) || screenWidth < 768
-const isTablet = /Tablet/i.test(userAgent) || (screenWidth >= 768 && screenWidth < 1024);
-// 从根节点开始遍历整个 DOM 树
-if (!isMobile) traverse(document.querySelector('#main-content'));
+    });
+    
+    const versionTabs = document.querySelectorAll('.version-tabs .tab');
+    const versionCards = document.querySelectorAll('.version-card');
+    
+    versionTabs.forEach(tab => {
+        tab.addEventListener('click', function() {
+            versionTabs.forEach(t => t.classList.remove('active'));
+            this.classList.add('active');
+            
+            const tabType = this.dataset.tab;
+            
+            versionCards.forEach(card => {
+                if (card.dataset.versionType === tabType) {
+                    card.style.display = 'block';
+                } else {
+                    card.style.display = 'none';
+                }
+            });
+        });
+    });
+    
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
